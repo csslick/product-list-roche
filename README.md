f6beca8050e048c06e8d91ac48a786ccf2780c25
csslick <digitone@naver.com>
2021-10-03 21:33:38 +0900

    tab menu 필터링

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # product-list roche
 ## 제품색인검색
 1. 제품 data 구조 정의 및 fetching
-2. tab menu 필터링(미구현)
+2. tab menu 필터링
 3. 검색(미구현)
 4. 상세페이지(미구현)
--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
-window.onload = function() {
-  
-  const productList = document.querySelector('.product-list')
-  let html = '';
+// 제품 출력
+const productList = document.querySelector('.product-list')
+let html = '';
 
+function showProducts(categoryName) {
+  html = '';
   data.forEach(function(item){
-    console.log(item.name)
-    html += `
-      <li>
-        <a href="#">
-          <img src="${item.img}" alt="${item.name}">
-          <h3>${item.name}</h3>
-        </a>  
-      </li>
-    `
+    if(item.category === categoryName) {
+      console.log(item.name)
+      html += `
+        <li>
+          <a href="#">
+            <img src="${item.img}" alt="${item.name}">
+            <h3>${item.name}</h3>
+          </a>  
+        </li>
+      `
+    } else {
+      return;
+    }
   })
 
   productList.innerHTML = html;
+}
 
-} // end window.onload
+// 카테고리 버튼
+const tabBtn = document.querySelectorAll('.tabBtn');
+tabBtn.forEach(function(item){
+  item.addEventListener('click', function(e){
+    e.preventDefault();
+    const category = this.children[0].innerText;
+    showProducts(category);
+
+    // 탭버튼 선택 스타일
+    tabBtn.forEach(function(btn){
+      btn.classList.remove('active');
+    })
+    item.classList.add('active');
+
+  })
+})
+
+// 제품 출력
+showProducts('항암제');
